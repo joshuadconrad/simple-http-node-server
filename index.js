e32c7f767ac339eef260e9710f774c752fc1a0bd
joshuadconrad <joshuadconrad@gmail.com>
2019-07-10 21:19:12 -0500

    chekpoint submission

---
+++ index.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const http = require('http');
+const port = 3000;
+
+// Handles HTTP requests.
+const requestHandler = (request, response) => {
+  fs.writeFile('hello-world.txt', 'Hello to this great world', 'utf8', (err) => {
+    if (err) throw err;
+    console.log('success');
+    response.end(`Handling a request on port ${port}`)
+  });
+};
+
+// Create a server and pass in the  requestHandler function
+const server = http.createServer(requestHandler);
+
+// Start the server listening on port 3000
+server.listen(port, (err) => {
+  if (err) {
+    return console.log(`You have an error:  ${err}`);
+  }
+
+  console.log(`server is listening on ${port}`);
+});
+
+http.get("http://localhost:3000", (resp) => {
+  let data = '';
+
+  // A chunk of data has been recieved.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    console.log(JSON.parse(data).explanation);
+  });
+
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
